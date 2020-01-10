96654e5e6b03cc28191815d059de3a677733a8aa
Vivek Sharma <2017ugcs071@nitjsr.ac.in>
2020-01-11 04:24:25 +0530

    ojass-3rdBuild

---
+++ Team/pages/page6.php
@@ -0,0 +1,142 @@
+<!-- <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css" /> -->
+<link rel="stylesheet" href="style.css">
+
+
+  
+<div class="row" style="display:flex;flex-direction:row;justify-content:space-between;">
+    
+    <div class="col-md-4">
+    <div  style="margin:10px;width:220px;">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://drive.google.com/uc?id=1xmQlCjgCdwxTLs9f9FDw-dENJYNdpjIw">
+                        <ul class="social">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <!-- <li><a href="#" class="fa fa-twitter"></a></li> -->
+                            <li><a href="#" class="fa fa-instagram"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                        </ul>
+                    </div>
+                    <div class="team-content">
+                        <h3 class="title">Mansi</h3>
+                        <!-- <span class="post">Web Developer</span> -->
+                    </div>
+                </div>
+     </div>
+    </div>
+   
+     <div class="col-md-4">
+       <div style="margin:10px;width:220px;">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://drive.google.com/uc?id=19qkFRZQTLTEDxKCRvDWs-Vr1vnrYe7RX">
+                        <ul class="social">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <!-- <li><a href="#" class="fa fa-twitter"></a></li> -->
+                            <li><a href="#" class="fa fa-instagram"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                        </ul>
+                    </div>
+                    <div class="team-content">
+                        <h3 class="title">Sandeep Yadav</h3>
+                        <!-- <span class="post">Web Developer</span> -->
+                    </div>
+                </div>
+     </div>
+     </div>
+   
+     <div class="col-md-4">
+       <div  style="margin:10px;width:220px;">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://drive.google.com/uc?id=19qkFRZQTLTEDxKCRvDWs-Vr1vnrYe7RX">
+                        <ul class="social">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <!-- <li><a href="#" class="fa fa-twitter"></a></li> -->
+                            <li><a href="#" class="fa fa-instagram"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                        </ul>
+                    </div>
+                    <div class="team-content">
+                        <h3 class="title">Ankit Makhija</h3>
+                        <!-- <span class="post">Web Developer</span> -->
+                    </div>
+                </div>
+     </div>
+     </div>
+   </div>
+
+   <div class="row" style="display:flex;flex-direction:row;justify-content:space-between;margin-left:12%;">
+
+   <div class="col-md-6">
+   <div style="margin:10px;width:220px;">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://drive.google.com/uc?id=14bCQ8Rx8Hm0JoztlxSeScCi6_7R4sfpv">
+                        <ul class="social">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <!-- <li><a href="#" class="fa fa-twitter"></a></li> -->
+                            <li><a href="#" class="fa fa-instagram"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                        </ul>
+                    </div>
+                    <div class="team-content">
+                        <h3 class="title">Malikarjun</h3>
+                        <!-- <span class="post">Web Developer</span> -->
+                    </div>
+                </div>
+     </div>
+   
+   
+   
+   </div>
+
+   
+   <div class="col-md-6">
+   <div style="margin:10px;width:220px;">
+                <div class="our-team">
+                    <div class="pic">
+                        <img src="https://drive.google.com/uc?id=19qkFRZQTLTEDxKCRvDWs-Vr1vnrYe7RX">
+                        <ul class="social">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <!-- <li><a href="#" class="fa fa-twitter"></a></li> -->
+                            <li><a href="#" class="fa fa-instagram"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                        </ul>
+                    </div>
+                    <div class="team-content">
+                        <h3 class="title">Uday Kumar Reddy</h3>
+                        <!-- <span class="post">Web Developer</span> -->
+                    </div>
+                </div>
+     </div>
+   
+   
+   
+   </div>
+   
+   
+   </div>
+
+
+
+
+
+
+
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- <script type="text/javascript" src="slick/slick.js"></script> -->
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $(".your-class").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      lazyLoad: 'ondemand',
+    });
+  });
+</script>
